ba9081a67c84021c5d5c5c28e1e8f35659794a29
sachetsingh77 <sachet@leverj.io>
2024-12-20 10:50:08 +0530

    initial

--- packages/server/middle/routes/api/nse/publish.js
+++ packages/server/middle/routes/api/nse/publish.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const baseUrl = "http://localhost:3000"
 const appRecvUrl = "/api/nse/receive"
-const dataToPost = require('./output.json')
+//const dataToPost = require('./output.json')
 
 const route = async (req, res) => {
     let mesg = ''
     try{
-        await axios.post(baseUrl + appRecvUrl, dataToPost)
+        await axios.post(baseUrl + appRecvUrl, req.body)
         mesg = 'PUBLISHED'
     }
     catch(e){
