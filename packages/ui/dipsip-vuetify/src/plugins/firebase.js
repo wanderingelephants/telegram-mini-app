4aebefbf17dd4a9612aa0fdd70bcccb1b9188073
sachetsingh77 <sachet@leverj.io>
2025-01-19 15:52:46 +0530

    firebase prod

--- packages/ui/dipsip-vuetify/src/plugins/firebase.js
+++ packages/ui/dipsip-vuetify/src/plugins/firebase.js
@@ -1,11 +1,7 @@
 // src/plugins/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-console.log(process.env.NODE_ENV)
-const firebaseConfig = process.env.NODE_ENV === 'production' 
-  ? import('../config/firebase.config.prod.json')
-  : import('../config/firebase.config.dev.json');
-console.log("firebaseConfig", firebaseConfig)
+import firebaseConfig from '../config/firebase.json';
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
