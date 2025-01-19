b52cb5b032b0e41def1c41d38f59714262e6ae21
sachetsingh77 <sachet@leverj.io>
2025-01-19 15:34:52 +0530

    firebase prod

---
+++ packages/ui/dipsip-vuetify/dist/assets/firebase.config.prod-CMcj8fXD.js
@@ -0,0 +1,1 @@
+const o="AIzaSyDyiQFCP1i5Bx3M4-pTkDvh1WvEgRFRldY",i="productiondipsip.firebaseapp.com",p="productiondipsip",t="764195462908-1ikt9j2sprusmseeiov5sn7k1pb1dj37.apps.googleusercontent.com",e={apiKey:o,authDomain:i,projectId:p,clientId:t};export{o as apiKey,i as authDomain,t as clientId,e as default,p as projectId};
