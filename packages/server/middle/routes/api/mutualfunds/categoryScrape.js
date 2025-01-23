bb51c58a0faeb15d319083084f79ef3ae8b17bdc
sachetsingh77 <sachet@leverj.io>
2025-01-23 15:16:57 +0530

    category scrape

--- packages/server/middle/routes/api/mutualfunds/categoryScrape.js
+++ packages/server/middle/routes/api/mutualfunds/categoryScrape.js
@@ -2,7 +2,7 @@ const {getAllMutualFunds} = require('../../../mf/moneycontrolscraper')
 const route = async(req, res) => {
     try{
         const categoryList = req.query.categoryList
-        const categories = categoryList.split(",")
+        const categories = categoryList ? categoryList.split(",") : []
         const result = await getAllMutualFunds(categories)
         res.status(200).json(result)
     }
