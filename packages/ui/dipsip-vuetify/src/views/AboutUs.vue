82c40ab30392f912e8f17f7dbaa091f3297e6965
sachetsingh77 <sachet@leverj.io>
2025-02-13 19:28:20 +0530

    updated about us

--- packages/ui/dipsip-vuetify/src/views/AboutUs.vue
+++ packages/ui/dipsip-vuetify/src/views/AboutUs.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-       <h2> AIdea Solutions Pvt. Ltd. is a Bengaluru based tech start-up. Our ambition is to bring to general investors tools, that bring huge productivity gains, similar to what ChatGPT and Claude have done for programmers.</h2>
-       <h3>Enterprise Customer ? Want to build a productivity tool like DipSip on your propeitary data ? Talk to us. DipSip is a self-hosted small language model / AI Agent system, where internal data is never posted to external LLMs.</h3>
+       <h2> AIdea Solutions Pvt. Ltd. is a Bengaluru based tech start-up. Our ambition is to bring to general investors tools, similar to what ChatGPT and Claude have done for programmers.</h2>
+       <h3>Enterprise Customer ? Want to build an AI toolkit like DipSip on your propeitary data ? Talk to us. DipSip is a self-hosted small language model / AI Agent system, where internal data is never posted to external LLMs.</h3>
     </div>
 </template>
