4a60d252b4773a2992ddc4dd2c46135b973f931a
sachetsingh77 <sachet@leverj.io>
2025-02-14 20:35:03 +0530

    automate summaries

--- packages/server/middle/routes/api/nse/summaries.js
+++ packages/server/middle/routes/api/nse/summaries.js
@@ -15,7 +15,7 @@ const route = async(req, res) => {
     const announcement_data_folder = process.env.NSE_ANNOUNCEMENTS_DOWNLOAD
     const toks = summaryDate.split("-")
     const formattedDate = toks[2] + "/" + toks[1] + "/" + toks[0]
-    await axios.post(process.env.PDF_PROCESS_URL + `/api/processPDFs?inputFolder=${formattedDate}&outputFolder=${formattedDate}`)
+    await axios.get(process.env.PDF_PROCESS_URL + `/api/processPDFs?inputFolder=${formattedDate}&outputFolder=${formattedDate}`)
     await processSummaries(announcement_data_folder+"/to_text/" + formattedDate, announcement_data_folder+"/summaries/" + formattedDate)
     res.status(200).json("ok")
 }
