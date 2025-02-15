99b94f09b0041608f7189dfdff565760c45bd4b6
sachetsingh77 <sachet@leverj.io>
2025-02-15 10:17:15 +0530

    automate summaries

--- packages/server/middle/index.js
+++ packages/server/middle/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 const ProcessETFQuotes = require('./routes/api/nse/processETFQuotes')
 const route = require("./routes/api/kite/instrument/eod")
 const { promisify } = require('util');
-
+const summaryServiceUrl = process.env.SUMMARY_SERVICE_URL
 
 /*cron.schedule('0 12 * * 1-5', async () => {
   console.log('running a task during market hours of NSE', new Date());
@@ -24,20 +24,29 @@ cron.schedule('32 15 * * 1-5', async () => {
   let res = {status: (code) => {console.log(code)}, json: (msg) => {console.log(msg)}}
   await route(req, res)  
 }, {timezone: "Asia/Kolkata"});
-cron.schedule('5 0 * * *', async () => {
+cron.schedule('35 10 * * *', async () => {
   try {
+    const t1 = Date.now(); 
+    if (!summaryServiceUrl) {
+      console.log("Summary Service url not found")
+      return;
+    }
     // Compute yesterday's date
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
     const formattedDate = yesterday.toLocaleDateString('en-GB').split('/').join('-'); // Converts to dd-mm-yyyy
-    let summaryUrl = `${process.env.SUMMARY_SERVICE_URL}/api/nse/summaries?summaryDate=${formattedDate}&index=sme`;
+    let summaryUrl = `${summaryServiceUrl}/api/nse/summaries?summaryDate=${formattedDate}&index=sme`;
     console.log(`Invoking: ${summaryUrl}`);
     let response = await axios.get(summaryUrl);
+    const t2 = Date.now(); // End time
     console.log('SME Summary API response:', response.data);
-    summaryUrl = `${process.env.SUMMARY_SERVICE_URL}/api/nse/summaries?summaryDate=${formattedDate}&index=equities`;
+    console.log(`SME Execution time: ${(t2 - t1)} ms`); 
+    summaryUrl = `${summaryServiceUrl}/api/nse/summaries?summaryDate=${formattedDate}&index=equities`;
     console.log(`Invoking: ${summaryUrl}`);
     response = await axios.get(summaryUrl);
     console.log('Equities Summary API response:', response.data);
+    const t3 = Date.now();
+    console.log(`Equities Execution time: ${(t3 - t2)} ms`); // Log time taken
   } catch (error) {
     console.error('Error calling summary API:', error);
   }
