41c7afbd37cc00065a91859a55a4da2120e17b3a
sachetsingh77 <sachet@leverj.io>
2025-02-18 20:32:18 +0530

    analysis reasoning

--- packages/server/hasura/migrations/1739780199116_alter_table_public_portfolio_mutual_funds_add_unique_user_id_mutual_fund_id/up.sql
+++ packages/server/hasura/migrations/1739780199116_alter_table_public_portfolio_mutual_funds_add_unique_user_id_mutual_fund_id/up.sql
@@ -1,1 +1,2 @@
+alter table "public"."portfolio_mutual_funds" drop constraint "portfolio_mutual_funds_user_id_mutual_fund_id_key";
 alter table "public"."portfolio_mutual_funds" add constraint "portfolio_mutual_funds_user_id_mutual_fund_id_key" unique ("user_id", "mutual_fund_id");
