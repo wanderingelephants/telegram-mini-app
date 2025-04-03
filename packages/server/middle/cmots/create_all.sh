b4969a32947052d5a5e6a57922d1b67fe84fb99a
sachetsingh77 <sachet@leverj.io>
2025-04-03 20:10:15 +0530

    updated cmots

--- packages/server/middle/cmots/create_all.sh
+++ packages/server/middle/cmots/create_all.sh
@@ -1,6 +1,6 @@
 DATA_ROOT_FOLDER=/Users/sachetsingh1/telegram-mini-app/packages/data
-#node csv_to_json.js /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/csv/tables_all.csv /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/json
-#node csv_to_json.js /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/csv/tables_financials.csv /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/json
+node csv_to_json.js /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/csv/tables_all.csv /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/json
+node csv_to_json.js /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/csv/tables_financials.csv /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/json
 node csv_to_json.js /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/csv/tables_price_volume.csv /Users/sachetsingh1/telegram-mini-app/packages/server/middle/cmots/json
 #first put master files for referential integrity
 jq -s '[.[][]]' json/company_index_master.json json/company_sector_master.json json/company_master.json $(ls json/*.json | grep -v '_master.json' | grep -v 'all_tables.json') > json/all_tables.json
